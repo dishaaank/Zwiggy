4c1d4a359b2d2d6b8f7d44bb1331d2f630e4191f
Dishank Agrawal <contactdishank123@gmail.com>
2024-05-22 02:12:47 +0530

    first commit

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+const Home = () => {
+    const [category, setcategory] = useState("All");
+  return (
+    <div>
+      <Header></Header>
+      <ExploreMenu category={category} setcategory={setcategory}></ExploreMenu>
+      <FoodDisplay category={category}></FoodDisplay>
+    </div>
+  )
+}
+
+export default Home;

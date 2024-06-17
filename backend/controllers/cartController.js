f3e9198591ea5690685662ea984bb889f93ba2dd
Dishank Agrawal <contactdishank123@gmail.com>
2024-06-17 14:31:58 +0530

    internal change

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -1,49 +1,62 @@
 import userModel from "../models/userModel.js";
 
-const addToCart = async (req,res)=>{
+const addToCart = async (req, res) => {
     try {
         let userData = await userModel.findById(req.body.userId);
-        let cartData = await userData.cartData;
-        if (!cartData[req.body.itemId])
-        {
-            cartData[req.body.itemId] = 1;
+        if (userData) {
+            if (!userData.cartData) {
+                userData.cartData = {};
+            }
+            if (!userData.cartData[req.body.itemId]) {
+                userData.cartData[req.body.itemId] = 1;
+            } else {
+                userData.cartData[req.body.itemId] += 1;
+            }
+            await userData.save();
+            res.json({ success: true, message: "Added to cart" });
+        } else {
+            res.json({ success: false, message: "User not found" });
         }
-        else{
-            cartData[req.body.itemId] += 1;
-        }
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData})
-        res.json({success:true,message:"Added to cart"});
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
 }
 
-const removeFromCart = async (req,res) => {
+const removeFromCart = async (req, res) => {
     try {
         let userData = await userModel.findById(req.body.userId);
-        let cartData = await userData.cartData;
-        if (cartData[req.body.itemId]>0){
-            cartData[req.body.itemId] -= 1;
+        if (userData) {
+            if (userData.cartData && userData.cartData[req.body.itemId] > 0) {
+                userData.cartData[req.body.itemId] -= 1;
+                if (userData.cartData[req.body.itemId] === 0) {
+                    delete userData.cartData[req.body.itemId];
+                }
+            }
+            await userData.save();
+            res.json({ success: true, message: "Removed from cart" });
+        } else {
+            res.json({ success: false, message: "User not found" });
         }
-        await userModel.findByIdAndUpdate(req.body.userId,{cartData});
-        res.json({success:true,message:"Removed from cart"})
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
-        
+        res.json({ success: false, message: "Error" });
     }
 }
 
-const getCart = async (req,res) => {
+const getCart = async (req, res) => {
     try {
         let userData = await userModel.findById(req.body.userId);
-        let cartData = await userData.cartData;
-        res.json({success:true,cartData})
+        if (userData) {
+            let cartData = userData.cartData || {}; // Initialize cartData to an empty object if it doesn't exist
+            res.json({ success: true, cartData });
+        } else {
+            res.json({ success: false, message: "User not found" });
+        }
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error"})
+        res.json({ success: false, message: "Error" });
     }
 }
 
-export {addToCart,removeFromCart,getCart}
+export { addToCart, removeFromCart, getCart }
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
-import { useContext } from 'react'
-import { useState } from 'react'
-import { StoreContext } from '../../context/StoreContext'
-const FoodItem = ({id,name,price,description,image}) => {
-    const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext);
+import React from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext';
 
-  return (
-    
-    <div className='food-item'>
-    <div className="food-item-img-container">
-        <img className='food-item-image' src={url+"/images/"+image} alt="" />
-        {!cartItems[id]
-                ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt="" />
-                :<div className='food-item-counter'>
-                  <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt='' />
-                  <p className='cartitemsp'>{cartItems[id]}</p>
-                  <img onClick={()=>addToCart(id)} src={assets.add_icon_green} alt='' />
-                  </div>
-            }
-            </div>
-            
+const FoodItem = ({ id, name, price, description, image }) => {
+    const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p className='namewe'>{name}</p>
-                <img className='ratingstars' src={assets.rating_starts} alt="" />
+    // Check if cartItems is defined and has the id property
+    const itemInCart = cartItems && cartItems[id];
+
+    return (
+        <div className='food-item'>
+            <div className="food-item-img-container">
+                <img className='food-item-image' src={url + "/images/" + image} alt="" />
+                {!itemInCart ? (
+                    <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
+                ) : (
+                    <div className='food-item-counter'>
+                        <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt='' />
+                        <p className='cartitemsp'>{itemInCart}</p>
+                        <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt='' />
+                    </div>
+                )}
             </div>
-            <p className="food-item-desc">{description}</p>
-            <p className="food-item-price">${price}</p>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p className='namewe'>{name}</p>
+                    <img className='ratingstars' src={assets.rating_starts} alt="" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className="food-item-price">${price}</p>
+            </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default FoodItem
+export default FoodItem;
